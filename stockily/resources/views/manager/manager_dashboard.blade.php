65657fff83cff6737cca9abaa5ff6456b0c4817d
nesrine <nesrine.abdelhak@ensia.edu.dz>
2023-04-20 22:43:29 +0100

    updated version for first sprint of stockily

---
+++ stockily/resources/views/manager/manager_dashboard.blade.php
@@ -0,0 +1,24 @@
+@extends('/layouts.app')
+
+@section('title', 'Stockily | Warehouse Manager')
+
+@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Stockily | manager </title>
+</head>
+<body>
+    <main class="main__container">
+    <div class="main__left">
+     <h1>WELCOME Manager | ADD YOUR ITEMS HERE</h1>
+    </div>
+    </main>
+</body>
+</html>
+{{-- this page will be added in the next sprint --}}
+</section>
+@endsection
